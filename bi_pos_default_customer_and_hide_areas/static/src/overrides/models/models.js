131940f92210adc4fd57d65735b7ab88ba862445
Erick Denis <erickmercadooudalova@gmail.com>
2025-03-10 19:42:44 +0300

    comente el estilo

--- bi_pos_default_customer_and_hide_areas/static/src/overrides/models/models.js
+++ bi_pos_default_customer_and_hide_areas/static/src/overrides/models/models.js
@@ -7,7 +7,7 @@ patch(Order.prototype, {
 
     setup() {
         super.setup(...arguments);
-        
+        this.cosas_visuales = false;
         var default_customer = this.pos.config.res_partner_id;
         var default_customer_by_id = this.pos.db.get_partner_by_id(default_customer[0]);
         
@@ -43,9 +43,11 @@ patch(Order.prototype, {
     async camposDeLaVistaTres(){
         const h1Elements = document.querySelectorAll('h1'); // Selecciona todos los <h1>
         const count = [...h1Elements].filter(h1 => h1.textContent.trim() === 'Pago exitoso').length;
-
+        if(this.cosas_visuales == false){
+            this.cosas_visuales = true;
+        }
         const buttonValidate = document.querySelector('.button.next.validation');
-        if (buttonValidate && count === 1) {
+        if (buttonValidate && count === 1 && cosas_visuales) {
             const campo_receipt = document.querySelector('.pos-receipt-container');
             if(campo_receipt){
                 campo_receipt.setAttribute('style', 'display: none !important;');
@@ -64,7 +66,7 @@ patch(Order.prototype, {
                 //campo_monto.setAttribute('style', 'display: none !important;');
             }
             buttonValidate.addEventListener('click', () => {
-                debugger
+                this.cosas_visuales = false;
                 this.changeTextHeader("Gracias por su compra", "POR FAVOR, ESCANEE SUS PRODUCTOS");
             });
             //buttonValidate.setAttribute('style', 'display: none !important;');
@@ -72,7 +74,7 @@ patch(Order.prototype, {
         }else{
             //para tablets pequenas
             const buttonValidate1 = document.querySelector('.btn-switchpane.validation-button');
-            if(buttonValidate1 && count === 1){
+            if(buttonValidate1 && count === 1 && cosas_visuales){
                 const campo_receipt = document.querySelector('.pos-receipt-container');
                 if(campo_receipt){
                     campo_receipt.setAttribute('style', 'display: none !important;');
@@ -91,7 +93,7 @@ patch(Order.prototype, {
                     //campo_monto.setAttribute('style', 'display: none !important;');
                 }
                 buttonValidate1.addEventListener('click', () => {
-                    debugger
+                    this.cosas_visuales = false;
                     this.changeTextHeader("Gracias por su compra", "POR FAVOR, ESCANEE SUS PRODUCTOS");
                 });
                 //buttonValidate1.setAttribute('style', 'display: none !important;');
